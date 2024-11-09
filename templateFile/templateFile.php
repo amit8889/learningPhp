7f512123c55a09a6e05a91adf9e422b3952f5c31
Amit Verma <amitverma@jpfunware.com>
2024-11-09 14:55:30 +0530

    first commit

---
+++ templateFile/templateFile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>My First Webpage</title>
+    </head>
+    <body style="background-color: beige;">
+        <h1>Welcome to my first webpage</h1>
+        <p>This is a paragraph of text</p>
+        <p>This is another paragraph of text</p>
+        <p>This is yet another paragraph of text</p>
+        <p>This is the last paragraph of text</p>
+        <p>This is a paragraph of text with a link to <a href="https://www
+        .google.com">Google</a></p>
+        <p>This is a paragraph of text with a link to <a href="https://www
+        .facebook.com">Facebook</a></p>
+        <p>This is a paragraph of text with a link to <a href="https://www
+        .twitter.com">Twitter</a></p>
+
+
+    </body>
+</html>
